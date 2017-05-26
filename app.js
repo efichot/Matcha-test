dfc1813391a7d52497a14ad5377b714cbb400179
Etienne FICHOT <efichot@e1r4p6.42.fr>
2017-05-26 17:15:30 +0200

    nodemon added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('I love treehouse');
+	res.send('I love me');
 });
 
 app.listen(3333, () => console.log('Listening on port 3333'));
