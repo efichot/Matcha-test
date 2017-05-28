8500d003756c9a0f332bd7c30d610b18100eee48
Etienne Fichot <fichotetienne@gmail.com>
2017-05-28 15:32:06 +0200

    views and partials

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ import express from 'express';
 
 const app = express();
 
+// add pug into express
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
-	res.send('I love me');
+	const foo = {
+		name: 'Etienne',
+		age: 25
+	}
+	res.render('index', {etienne: foo});
 });
 
 app.listen(3333, () => console.log('Listening on port 3333'));
