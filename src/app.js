777929650bf9c566c1f374662202126d874c2587
Etienne FICHOT <efichot@e1r3p6.42.fr>
2017-05-29 17:05:59 +0200

    public folder

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from 'express';
 
 const app = express();
 
+//add static file (static server setup)
+app.use('/static', express.static(__dirname + '/public'));
+
 // add pug into express
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -10,9 +13,15 @@ app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
 	const foo = {
 		name: 'Etienne',
-		age: 25
+		age: 26
 	}
 	res.render('index', {etienne: foo});
 });
 
 app.listen(3333, () => console.log('Listening on port 3333'));
+
+
+const kek = {
+	nom: 'Etienne',
+	age: 26
+}
